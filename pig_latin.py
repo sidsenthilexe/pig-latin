83c2eb578725eb570d9f856317503b9335be1e26
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-28 07:59:04 -0800

    update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -1,16 +1,29 @@
 def main():
     sentence_input = input('Input your sentence: ')
     sentence_broken = list(sentence_input)
+    var = 0
+    
     if sentence_broken[0] in ['a', 'e', 'i', 'o', 'u']: 
         sentence_output = sentence_input+"ay"
     elif sentence_broken[1] in ['a', 'e', 'i', 'o', 'u']:
-        sentence_save = sentence_broken[0]
-        print(sentence_save)
-        del sentence_broken[0]
-        print(sentence_broken)
+        sentence_save = sentence_broken[0:1]
+        del sentence_broken[0:1]
+        save_assembled = ''.join(sentence_save)
         sentence_assembled = ''.join(sentence_broken)
-        print(sentence_assembled)
-        sentence_output = sentence_assembled + sentence_save + "a"
+        sentence_output = sentence_assembled + save_assembled + "ay"
+    elif sentence_broken[2] in ['a', 'e', 'i', 'o', 'u']:
+        sentence_save = sentence_broken[0:2]
+        del sentence_broken[0:2]
+        save_assembled = ''.join(sentence_save)
+        sentence_assembled = ''.join(sentence_broken)
+        sentence_output = sentence_assembled + save_assembled + "ay"
+    elif sentence_broken[3] in ['a', 'e', 'i', 'o', 'u']:
+        sentence_save = sentence_broken[0:3]
+        del sentence_broken[0:3]
+        save_assembled = ''.join(sentence_save)
+        sentence_assembled = ''.join(sentence_broken)
+        sentence_output = sentence_assembled + save_assembled + "ay"
     print(sentence_output)
+    
 
 main()
