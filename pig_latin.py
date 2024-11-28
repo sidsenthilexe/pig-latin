4c6c7494ebb175400c9e81208875c59dbb6cb188
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-27 17:42:40 -0800

    update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -1,8 +1,16 @@
-
-
 def main():
-    sentence_input = input("Input your sentence: ")
-    sentence_output = sentence_input+"ay"
+    sentence_input = input('Input your sentence: ')
+    sentence_broken = list(sentence_input)
+    if sentence_broken[0] in ['a', 'e', 'i', 'o', 'u']: 
+        sentence_output = sentence_input+"ay"
+    elif sentence_broken[1] in ['a', 'e', 'i', 'o', 'u']:
+        sentence_save = sentence_broken[0]
+        print(sentence_save)
+        del sentence_broken[0]
+        print(sentence_broken)
+        sentence_assembled = ''.join(sentence_broken)
+        print(sentence_assembled)
+        sentence_output = sentence_assembled + sentence_save + "a"
     print(sentence_output)
 
 main()
