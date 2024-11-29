7de42688c5d0a3447bed5455c6486408ad46b641
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-29 11:26:47 -0800

    update pig_latin.py - new method for parsing one word

--- pig_latin.py
+++ pig_latin.py
@@ -1,29 +1,32 @@
 def main():
     sentence_input = input('Input your sentence: ')
-    sentence_broken = list(sentence_input)
-    var = 0
+    sentence_split = sentence_input.split()
+    array_var = 0
     
-    if sentence_broken[0] in ['a', 'e', 'i', 'o', 'u']: 
-        sentence_output = sentence_input+"ay"
-    elif sentence_broken[1] in ['a', 'e', 'i', 'o', 'u']:
-        sentence_save = sentence_broken[0:1]
-        del sentence_broken[0:1]
-        save_assembled = ''.join(sentence_save)
-        sentence_assembled = ''.join(sentence_broken)
-        sentence_output = sentence_assembled + save_assembled + "ay"
-    elif sentence_broken[2] in ['a', 'e', 'i', 'o', 'u']:
-        sentence_save = sentence_broken[0:2]
-        del sentence_broken[0:2]
-        save_assembled = ''.join(sentence_save)
-        sentence_assembled = ''.join(sentence_broken)
-        sentence_output = sentence_assembled + save_assembled + "ay"
-    elif sentence_broken[3] in ['a', 'e', 'i', 'o', 'u']:
-        sentence_save = sentence_broken[0:3]
-        del sentence_broken[0:3]
-        save_assembled = ''.join(sentence_save)
-        sentence_assembled = ''.join(sentence_broken)
-        sentence_output = sentence_assembled + save_assembled + "ay"
-    print(sentence_output)
+    for term in sentence_split:
+        array_var += 1
+        word_broken = list(term)
+        if word_broken[0] in ['a', 'e', 'i', 'o', 'u']: 
+            word_output = sentence_input+"ay"
+        elif word_broken[1] in ['a', 'e', 'i', 'o', 'u']:
+            sentence_save = word_broken[0:1]
+            del word_broken[0:1]
+            save_assembled = ''.join(sentence_save)
+            sentence_assembled = ''.join(word_broken)
+            word_output = sentence_assembled + save_assembled + "ay"
+        elif word_broken[2] in ['a', 'e', 'i', 'o', 'u']:
+            sentence_save = word_broken[0:2]
+            del word_broken[0:2]
+            save_assembled = ''.join(sentence_save)
+            sentence_assembled = ''.join(word_broken)
+            word_output = sentence_assembled + save_assembled + "ay"
+        elif word_broken[3] in ['a', 'e', 'i', 'o', 'u']:
+            sentence_save = word_broken[0:3]
+            del word_broken[0:3]
+            save_assembled = ''.join(sentence_save)
+            sentence_assembled = ''.join(word_broken)
+            word_output = sentence_assembled + save_assembled + "ay"
+        print(word_output)
     
 
 main()
