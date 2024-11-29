dff14fa3933e1900b33fd2b1704cf8dc8bb809ef
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-29 11:47:22 -0800

    update pig_latin.py and save old method in temp.py

--- pig_latin.py
+++ pig_latin.py
@@ -7,27 +7,17 @@ def main():
     for term in sentence_split:
         array_var += 1
         word_broken = list(term)
-        if word_broken[0] in ['a', 'e', 'i', 'o', 'u']: 
-            word_output = sentence_input+"ay"
-        elif word_broken[1] in ['a', 'e', 'i', 'o', 'u']:
-            sentence_save = word_broken[0:1]
-            del word_broken[0:1]
-            save_assembled = ''.join(sentence_save)
-            word_assembled = ''.join(word_broken)
-            word_output = word_assembled + save_assembled + "ay"
-        elif word_broken[2] in ['a', 'e', 'i', 'o', 'u']:
-            sentence_save = word_broken[0:2]
-            del word_broken[0:2]
-            save_assembled = ''.join(sentence_save)
-            word_assembled = ''.join(word_broken)
-            word_output = word_assembled + save_assembled + "ay"
-        elif word_broken[3] in ['a', 'e', 'i', 'o', 'u']:
-            sentence_save = word_broken[0:3]
-            del word_broken[0:3]
-            save_assembled = ''.join(sentence_save)
-            word_assembled = ''.join(word_broken)
-            word_output = word_assembled + save_assembled + "ay"
+        for word_term in word_broken:
+            if word_term in ['a', 'e', 'i', 'o', 'u']:
+                word_term_index = word_broken.index(word_term)
+                sentence_save = word_broken[0:word_term_index]
+                del word_broken[0:word_term_index]
+                save_assembled = ''.join(sentence_save)
+                word_assembled = ''.join (word_broken)
+                word_output = word_assembled+save_assembled+"ay"
+                
         final_sentence.insert(array_var, word_output)
+        
     final_sentence_assembled = ' '.join(final_sentence)
     print(final_sentence_assembled)
 
