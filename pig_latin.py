509dd94fa274c1e151f422a709f5d3d012d4cf03
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-29 11:33:14 -0800

    script now parses a full sentence properly

--- pig_latin.py
+++ pig_latin.py
@@ -1,7 +1,8 @@
 def main():
     sentence_input = input('Input your sentence: ')
     sentence_split = sentence_input.split()
-    array_var = 0
+    final_sentence = []
+    array_var = -1
     
     for term in sentence_split:
         array_var += 1
@@ -12,21 +13,22 @@ def main():
             sentence_save = word_broken[0:1]
             del word_broken[0:1]
             save_assembled = ''.join(sentence_save)
-            sentence_assembled = ''.join(word_broken)
-            word_output = sentence_assembled + save_assembled + "ay"
+            word_assembled = ''.join(word_broken)
+            word_output = word_assembled + save_assembled + "ay"
         elif word_broken[2] in ['a', 'e', 'i', 'o', 'u']:
             sentence_save = word_broken[0:2]
             del word_broken[0:2]
             save_assembled = ''.join(sentence_save)
-            sentence_assembled = ''.join(word_broken)
-            word_output = sentence_assembled + save_assembled + "ay"
+            word_assembled = ''.join(word_broken)
+            word_output = word_assembled + save_assembled + "ay"
         elif word_broken[3] in ['a', 'e', 'i', 'o', 'u']:
             sentence_save = word_broken[0:3]
             del word_broken[0:3]
             save_assembled = ''.join(sentence_save)
-            sentence_assembled = ''.join(word_broken)
-            word_output = sentence_assembled + save_assembled + "ay"
-        print(word_output)
-    
+            word_assembled = ''.join(word_broken)
+            word_output = word_assembled + save_assembled + "ay"
+        final_sentence.insert(array_var, word_output)
+    final_sentence_assembled = ' '.join(final_sentence)
+    print(final_sentence_assembled)
 
 main()
